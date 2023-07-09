aae038eab737d3e12ea38879c2f812557108938c
Henrique Pereira <hpereira0408@gmail.com>
2023-07-09 13:48:32 +0100

    Implementando puppeteer

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.post("/sendMessages", async (req, res) => {
   try {
@@ -15,7 +17,7 @@ app.post("/sendMessages", async (req, res) => {
       });
     }
 
-    console.log("req.body", req.body);
+    const customers = req.body.customers;
 
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -24,21 +26,58 @@ app.post("/sendMessages", async (req, res) => {
     page.setDefaultTimeout(0);
 
     await page.waitForSelector('[data-testid="menu-bar-chat"]');
-    await page.click('[data-testid="menu-bar-chat"]');
-    await delay(2000);
 
-    await page.waitForSelector('[data-testid="chat-list-search"]');
-    await page.type('[data-testid="chat-list-search"]', "3187339163");
-    await delay(2000);
+    for (const customer of customers) {
+      const message = `Olá ${customer.nome}, Jato de Areia agradece  a nossa parceria. Segue nossa tabela!`;
 
-    await page.waitForSelector('[data-testid="list-item-1"]');
-    await page.click('[data-testid="list-item-1"]');
-    await delay(2000);
+      await page.click('[data-testid="menu-bar-chat"]');
+      await delay(2000);
 
-    res.send("Hello World!");
+      await page.waitForSelector('[data-testid="chat-list-search"]');
+      await page.type('[data-testid="chat-list-search"]', customer.whatsapp);
+      await delay(2000);
+
+      await page.waitForSelector(
+        '[data-testid="contact-list-key"] > div > div > div > [role="button"]'
+      );
+      await page.click(
+        '[data-testid="contact-list-key"] > div > div > div > [role="button"]'
+      );
+
+      await page.waitForSelector(
+        '[data-testid="conversation-compose-box-input"]'
+      );
+      await page.type(
+        '[data-testid="conversation-compose-box-input"]',
+        message
+      );
+      await delay(2000);
+      await page.waitForSelector('[data-testid="send"]');
+      await page.click('[data-testid="send"]');
+
+      await page.waitForSelector('[data-testid="conversation-clip"]');
+      await page.click('[data-testid="conversation-clip"]');
+      await delay(2000);
+
+      await page.waitForSelector('[data-testid="attach-document"]');
+      await delay(2000);
+
+      const [fileChooser] = await Promise.all([
+        page.waitForFileChooser(),
+        page.click('[data-testid="attach-document"]'),
+      ]);
+      await fileChooser.accept(["./public/imagens/Tabela.png"]);
+      await delay(2000);
+
+      await page.waitForSelector('[data-testid="send"]');
+      await page.click('[data-testid="send"]');
+      await delay(2000);
+    }
+
+    res.send({ message: "ok" });
   } catch (err) {
     console.log(err);
   }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
